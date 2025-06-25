d407ba291f701cf9dadb9aba318a22216fe5aaad
abdulsuhailks <abdulsuhailks26@gmail.com>
2025-06-26 04:05:49 +0530

    Final commit

--- bayut_scraper/spiders/scrape_bayut.py
+++ bayut_scraper/spiders/scrape_bayut.py
@@ -13,7 +13,6 @@ class BayutSpider(scrapy.Spider):
             full_url = response.urljoin(link)
             yield scrapy.Request(full_url, callback=self.parse_property)
 
-        # Follow next page (pagination)
         next_page = response.xpath('//a[@title="Next"]/@href').get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
@@ -28,52 +27,41 @@ class BayutSpider(scrapy.Spider):
 
         item = BayutItem()
 
-        # Property ID
         item['property_id'] = extract('//span[contains(text(), "Bayut -")]/text()')
 
-        # URL
         item['property_url'] = response.url
 
-        # Purpose, Type, Added On, Furnishing, Permit No.
         item['purpose'] = extract('//span[@aria-label="Purpose"]/text()'),
         item['type'] = extract('//span[@aria-label="Type"]/text()'),
         item['added_on'] = extract('//span[@aria-label="Reactivated date"]/text()'),
         item['furnishing'] = extract('//span[@aria-label="Furnishing"]/text()'),
         # item['permit_number'] = extract('//span[@aria-label="Purpose"]/text()'),
 
-        # Agent name
         item['agent_name'] = extract('//a[@aria-label="Agent name"]/text()'),
 
-        # Price
         item['price'] = {
             "currency": "AED",
             "amount": extract('//span[@aria-label="Price"]/text()')
         }
 
-        # Location
         item['location'] = extract('//div[@aria-label="Property header"]/text()')
 
-        # Bedrooms, Bathrooms, Size
         item['bed_bath_size'] = {
             "bedrooms": extract('//span[@aria-label="Beds"]/text()'),
             "bathrooms": extract('//span[@aria-label="Baths"]/text()'),
             "size": extract('//span[@aria-label="Area"]//text()')
         }
 
-        # Breadcrumbs
         item['breadcrumbs'] = ' > '.join(extract_list('//ol[@aria-label="Breadcrumb"]/li/a/text()'))
 
-        # Amenities
         item['amenities'] = extract_list('//div[@aria-label="Amenities"]/ul/li/text()')
 
-        # Description
         description = extract_list('//div[@aria-label="Property description"]//text()')
         item['description'] = ' '.join(description)
 
-        # Primary image
         item['primary_image_url'] = extract('(//img[contains(@alt, "Property image") or contains(@src, "thumbnails")])[1]/@src')
 
-        # All property images
         item['property_image_urls'] = extract_list('//div[@aria-label="Gallery"]//img/@src')
 
         yield item
+
